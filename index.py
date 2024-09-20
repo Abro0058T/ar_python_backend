41b46fdc2b7c1992361aebe315c7bb908f09382b
AbhiAtiUttam <Abhi@glocalize.net>
2024-09-20 13:23:47 +0530

    added locate furniture feature and combined wall and furniture feature  backend done

--- index.py
+++ index.py
@@ -1,25 +1,74 @@
-from flask import Flask
+from flask import Flask ,jsonify
 import cv2
 import numpy as np
 from flask import *
+
+from flask_cors import CORS, cross_origin
 from distutils.log import debug
 from fileinput import filename
 from ultralytics import YOLO
 
 app = Flask(__name__)
+CORS(app, resources={r"*": {"origins": "http://localhost:5173"}}, methods=["GET", "POST"], allow_headers=["Content-Type"])
+
+# app.config["CORS_HEADERS"] = 'Content-Type'
+# app.run(use_reloader=False)
 model = YOLO("best.pt")
 
 
 @app.route("/wall", methods=["POST"])
-def detect_lines_and_measure(image_path="./blueprint7.png"):
-    if request.method == "POST":
-        f = request.files["file"]
-        f.save(f.filename)
+@cross_origin()
+def detect_lines_and_measure(image_path="./blueprint4.png"):
+    # response.headers.add("Access-Control-Allow-Origin", "*")
+    # if request.method == "POST":
+    #     f = request.files["file"]
+    #     f.save(f.filename)
 
-    image = cv2.imread(f.filename)
+    # image = cv2.imread(f.filename)
+    image = cv2.imread(image_path)
     if image is None:
         raise ValueError("Image not found or unable to load.")
+    
+    # results = model.predict(image_path)
+    results= model(image)
+    # print(len(results[0].boxes))
+    furnitureCoordinate = []
 
+    for furni in results[0].boxes:
+        # return f"{furni}"
+        print(f"{furni.xyxy} this is furni")
+        furnitureCoordinate.append({
+            "coordinate" : [int(np.array(furni.xyxy)[0][0]),int(np.array(furni.xyxy)[0][1]),int(np.array(furni.xyxy)[0][2]),int(np.array(furni.xyxy)[0][3])],
+            "name": int(np.array(furni.cls)[0])
+        })
+    furniture = {
+        "furnitureCount": len(results[0].boxes),
+        "furnitureCoordinate": furnitureCoordinate
+    }
+
+    # Get the results
+    boxes = results[0].boxes.xyxy  # Bounding boxes in (x1, y1, x2, y2) format
+    confidences = results[0].boxes.conf  # Confidence scores
+    labels = results[0].boxes.cls  # Class labels
+
+    # Class names (COCO dataset example)
+    # class_names = model.names
+
+    # Draw bounding boxes on the image
+    for box, confidence, label in zip(boxes, confidences, labels):
+        x1, y1, x2, y2 = map(int, box)
+        # class_name = class_names[int(label)]
+        color = (255, 255, 255)  # Color for bounding box (green)
+
+        # Draw the bounding box
+        cv2.rectangle(image, (x1+10, y1+10), (x2-10, y2-10), color, -1)
+
+        # Add label and confidence score
+        # text = f"{class_name} {confidence:.2f}"
+        # cv2.putText(image, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+
+    # walls 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
@@ -39,23 +88,42 @@ def detect_lines_and_measure(image_path="./blueprint7.png"):
         cv2.line(line_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
         length = np.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-        linesCo.append([x1, y1, x2, y2, length])
-    return f"{linesCo}"
+        linesCo.append([int(x1), int(y1), int(x2),int(y2), int(length)])
+
+    finalResult = {"wall": linesCo, "furniture": furnitureCoordinate}
+    print(f"{finalResult}")
+    return json.dumps(finalResult)
 
 
-@app.route("/furniture", methods=["POST"])
-def detect_furniture(image_path="./test.jpg"):
-    if request.method == "POST":
-        f = request.files["file"]
-        f.save(f.filename)
+@app.route("/", methods=["POST"])
+@cross_origin()
+def detect_furniture(image_path="./blueprint4.png"):
+    # if request.method == "POST":
+    #     f = request.files["file"]
+    #     f.save(f.filename)
 
-    image = cv2.imread(f.filename)
-    if image is None:
-        raise ValueError("Image not found or unable to load.")
-    print(image)
-    results = model.predict(image)
-    print(len(results[0].boxes))
-    return f"{results}"
+    # image = cv2.imread(f.filename)
+    # if image is None:
+    #     raise ValueError("Image not found or unable to load.")
+    # print(image)
+    # results = model.predict(f.filename)
+    results = model.predict(image_path)
+    # print(len(results[0].boxes))
+    furnitureCoordinate = []
+
+    for furni in results[0].boxes:
+        # return f"{furni}"
+        print(f"{furni.xyxy} this is furni")
+        furnitureCoordinate.append({
+            "coordinate" : [int(np.array(furni.xyxy)[0][0]),int(np.array(furni.xyxy)[0][1]),int(np.array(furni.xyxy)[0][2]),int(np.array(furni.xyxy)[0][3])],
+            "name": int(np.array(furni.cls)[0])
+        })
+    furniture = {
+        "furnitureCount": len(results[0].boxes),
+        "furnitureCoordinate": furnitureCoordinate
+    }
+    print(f"{furniture}")
+    return json.dumps(furniture)
 
 
 if __name__ == "__main__":
